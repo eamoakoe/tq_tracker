995fa6ffe19921e1ddbc1984923ed3b06e279cf9
eamoakoe <eamoako23@icloud.com>
2026-05-19 15:11:49 +0100

    Update Streamlit app

--- components/sidebar.py
+++ components/sidebar.py
@@ -1,17 +1,19 @@
-st.markdown('<div class="section-title">PROJECT</div>', unsafe_allow_html=True)
+import streamlit as st
 
-project_list = [
-    "Flass Lane",
-    "Ferry PS",
-    "Rossall Outfall",
-    "Tally Ho",
-    "Harbour Yard",
-    "Eccleston Bridge",
-    "Palace Nook",
-    "Rampside"
-]
+def render_sidebar():
+    st.markdown('<div class="section-title">PROJECT</div>', unsafe_allow_html=True)
 
-project = st.selectbox(
-    "",
-    project_list
-)
+    project_list = [
+        "Flass Lane",
+        "Ferry PS",
+        "Rossall Outfall",
+        "Tally Ho",
+        "Harbour Yard",
+        "Eccleston Bridge",
+        "Palace Nook",
+        "Rampside"
+    ]
+
+    project = st.selectbox("", project_list)
+
+    return project
