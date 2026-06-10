c8615c07e7bf8e5191756a2c230b129df3dec4f7
eamoakoe <yourusername@users.noreply.github.com>
2026-06-10 14:11:27 +0100

    Updated

--- components/sidebar.py
+++ components/sidebar.py
@@ -52,7 +52,7 @@ def render_sidebar(datasets):
                 font-weight:600;
                 margin-bottom:8px;
             ">
-                UU AMP8 DD&B
+                UU AMP8 DD&B FRAMEWORK
             </div>
         </div>
         """, unsafe_allow_html=True)
